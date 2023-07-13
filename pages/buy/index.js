7c4b2f69f1d8e674edcd1ac8b2805ee474d177f9
liu54764 <2806671947@qq.com>
2023-07-13 10:28:12 +0800

    3

--- pages/buy/index.js
+++ pages/buy/index.js
@@ -1,66 +1,45 @@
-// pages/buy/index.js
+// 页面逻辑部分的 JavaScript 文件
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    shopList: [
+      {
+        name: '水果店',
+        image: '/images/fruits.jpg',
+      },
+      {
+        name: '超市',
+        image: '/images/supermarket.jpg',
+      },
+    ],
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
+  onLoad() {
+    // 从后端获取店铺信息的示例，你需要根据实际情况进行调整
+    wx.request({
+      url: '后端接口地址',
+      success: (res) => {
+        if (res.statusCode === 200) {
+          const shopList = res.data;
+          this.setData({
+            shopList: shopList,
+          });
+        } else {
+          console.error('获取店铺信息失败');
+        }
+      },
+      fail: () => {
+        console.error('请求失败，请检查网络连接');
+      },
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
+  goToShopDetail(event) {
+    const shopIndex = event.currentTarget.dataset.index;
+    const selectedShop = this.data.shopList[shopIndex];
+    // 跳转到店铺商品页面，并传递选中店铺的信息
+    wx.navigateTo({
+      url: '/pages/shopDetail/shopDetail?shop=' + JSON.stringify(selectedShop),
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  }
-})
+});
